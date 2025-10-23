712a77ed580bc3b4a5024946b14894fec1eab6d0
Gabe-Faus <gabriel.faustino900@gmail.com>
2025-10-23 18:25:29 -0300

    Essa é a versão Nozama

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import { api, HydrateClient } from "@/trpc/server";
 
 
 
+
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
   const session = await auth();
@@ -17,33 +18,81 @@ export default async function Home() {
 
   return (
     <main>
-      <nav className="bg-green-950 h-20 text-white text-center font-mono text-xl font-bold flex items-center justify-center">
-        <a className="p-8 hidden md:block" href="#">About us</a>
-        <a className="p-8 hidden md:block" href="#">Pricing</a>
-        <a className="p-8 hidden md:block" href="#">Ratings</a>
+      <nav>
+        {/* Primeira navbar */}
+        <div className="bg-navbar h-15 flex items-center justify-between px-4">
+          
+          {/* Logo ou ZIP CODE */}
+          <div className="flex items-center gap-2 cursor-pointer">
+            <img src="/localPin.png" alt="Local Pin" className="max-h-[20px] hidden sm:block" />
+            <h2 className="hidden md:block text-white font-amazon">Send to your ZIP CODE</h2>
+          </div>
 
-        <button className="md:hidden text-3xl focus:outline-none">☰</button>
+          {/* Search Bar */}
+          <div className="flex-1 flex justify-center items-center px-2 cursor-pointer">
+            <div className="h-10 w-full max-w-[650px] bg-white text-black/70 flex items-center px-4">
+              Search Nozama.com.br
+            </div>
+            <div className="h-10 w-10 bg-amber-500 hover:bg-amber-600 flex justify-center items-center ">
+              <img src="/magnifier.png" alt="Magnifier" className="h-8" />
+            </div>
+          </div>
 
-        
+          {/* Orders and Returns and Trolley */}
+          <div className="flex items-center gap-4 cursor-pointer">
+            <h2 className="hidden md:block text-white">Orders and returns</h2>
+            <div className="hidden md:flex items-center gap-1">
+              <img src="/trolley.png" alt="Trolley" className="h-10" />
+              <h1 className="text-white">Trolley</h1>
+            </div>
+
+            {/* Button Menu */}
+            <button className="md:hidden text-3xl focus:outline-none text-white">☰</button>
+          </div>
+        </div>
+
+        {/* Segunda navbar */}
+        <div className="h-10 bg-scndnavbar"></div>
       </nav>
 
-      <section className="bg-[url('/amazon_img1.jpg')] bg-cover bg-center h-[650px]">
-        <div className="absolute top-1/2 right-10 -translate-y-1/2 bg-green-950/80 h-[500px] md:h-[400px] w-[400px] md:w-[700px] font-mono shadow-xl/30 flex flex-col justify-between">
 
-          <h1 className="p-6 text-3xl text-center text-white font-bold">"Your shortcut to honest product feedback"</h1>
+      
+      {/* Primeira página Gabriel Pessoa Faustino */}
+      <section className="bg-bodyamazon bg-cover bg-center h-[650px]">
+        <div className="bg-bodyamazon sm:bg-[url('/Personbox.jpg')] bg-cover bg-center h-[900px] w-[1521px] p-[30px]">
 
-          <h4 className="text-white p-6 text-center">Amazonia is the best place to find reviews of the products you like the most, here you can find out wich items are worth it, how-to-use guides and a large buyers community. </h4>
+          <div className="absolute top-3/5 right-10 -translate-y-1/2 bg-scndnavbar/80 h-[500px] md:h-[400px] w-[400px] md:w-[700px] font-amazon shadow-xl/30 flex flex-col justify-between">
+            <h1 className="p-6 text-3xl text-center text-white font-bold">
+              "Your shortcut to honest product feedback"
+            </h1>
 
-          <button className="self-center mb-6 bg-white shadow-xl-30 px-6 py-2 font-bold hover:bg-green-950 hover:text-white cursor-pointer text-black">Get started ↗</button>
+            <h4 className="text-white p-6 text-center">
+              Nozama is the best place to find reviews of the products you like the most, here you can find out wich items are worth it, how-to-use guides and a large buyers community.
+            </h4>
+
+            <button className="self-center mb-6 bg-white shadow-xl-30 px-6 py-2 font-bold hover:bg-green-950 hover:text-white cursor-pointer text-black">
+              Get started ↗
+            </button>
+          </div>
         </div>
       </section>
 
-      <section className="bg-white h-[730px]">
-        
+
+
+      <section className="bg-bodyamazon h-[730px]">
+       
       </section>
 
-      <footer className="bg-green-950 h-[50px] text-center flex items-center justify-center">
-        <h3 className="text-white font-mono font-bold">Created by Gabriel & Marina | All right's reserved ©</h3>
+
+      {/* Footer Gabriel Pessoa Faustino */}
+      <footer>
+        <div className="bg-scndnavbar h-[50px] text-center flex items-center justify-center">
+          <h3 className="text-white font- font-bold">Created by Gabriel & Marina | All right's reserved ©</h3>
+        </div>
+
+        <div className="bg-navbar h-[50px]">
+
+        </div>
       </footer>
     </main>
   );
